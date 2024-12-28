a2fde710e79bf5752137a3d7f61f582db76b6106
Danielle Bassetto <danibbassetto@hotmail.com>
2024-12-28 20:47:00 -0300

    feat(Aula 43.04)

---
+++ GalleryApp/Models/Component.cs
@@ -0,0 +1,8 @@
+﻿namespace GalleryApp.Models;
+
+public class Component
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public Page Page { get; set; }
+}
---
+++ GalleryApp/Repositories/CategoryRepository.cs
@@ -0,0 +1,29 @@
+﻿using GalleryApp.Models;
+using GalleryApp.Views.Layouts;
+
+namespace GalleryApp.Repositories;
+
+public class CategoryRepository
+{
+    public CategoryRepository() { }
+
+    public List<Category> GetCategories()
+    {
+        List<Category> categories =
+        [
+            new Category
+            {
+                Name = "Layout",
+                Components = [
+                    new Component             {
+                        Title = "StackLayout",
+                        Description = "Organização sequencial dos elementos.",
+                        Page = new StackLayoutPage()
+                    }
+                ]
+            },
+        ];
+
+        return categories;
+    }
+}
