9b30491d5bf009d04a9be072bba5a3c5c808445a
Danielle Bassetto <danibbassetto@hotmail.com>
2024-12-29 13:04:23 -0300

    feat(Aula 46.06)

--- GalleryApp/Views/Menu.xaml.cs
+++ GalleryApp/Views/Menu.xaml.cs
@@ -1,9 +1,64 @@
+using GalleryApp.Repositories;
+
 namespace GalleryApp.Views;
 
 public partial class Menu : ContentPage
 {
-	public Menu()
-	{
-		InitializeComponent();
-	}
+    public Menu()
+    {
+        InitializeComponent();
+
+        var categories = new CategoryRepository().GetCategories();
+
+        foreach (var category in categories)
+        {
+            var lblCategory = new Label
+            {
+                Text = category.Name,
+                FontFamily = "OpenSansSemibold",
+                TextColor = Colors.Black
+            };
+
+            MenuContainer.Children.Add(lblCategory);
+
+            foreach (var component in category.Components)
+            {
+                var tap = new TapGestureRecognizer();
+                tap.CommandParameter = component.Page;
+                tap.Tapped += OnTapComponent;
+
+                var lblComponentTitle = new Label
+                {
+                    Text = component.Title,
+                    FontFamily = "OpenSansSemibold",
+                    Margin = new Thickness(20, 20, 0, 0),
+                    TextColor = Colors.Black,
+                };
+
+                lblComponentTitle.GestureRecognizers.Add(tap);
+
+                var lblComponentDescription = new Label
+                {
+                    Text = component.Description,
+                    Margin = new Thickness(20, 0, 0, 0),
+                    TextColor = Colors.Black
+                };
+
+                lblComponentDescription.GestureRecognizers.Add(tap);
+
+                MenuContainer.Children.Add(lblComponentTitle);
+                MenuContainer.Children.Add(lblComponentDescription);
+            }
+        }
+    }
+
+    private void OnTapComponent(object sender, TappedEventArgs e)
+    {
+        var label = (Label)sender;
+        var tap = (TapGestureRecognizer)label.GestureRecognizers[0];
+        var page = (Page)tap.CommandParameter!;
+
+        ((FlyoutPage)App.Current!.MainPage!).Detail = page;
+        ((FlyoutPage)App.Current.MainPage).IsPresented = false;
+    }
 }
